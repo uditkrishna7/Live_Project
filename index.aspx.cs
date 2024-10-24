5fed735bfa7a122a3b7391e4f328067d8fdff8b9
uditkrishna7 <138179050+uditkrishna7@users.noreply.github.com>
2024-10-24 16:05:21 +0530

    check

--- index.aspx.cs
+++ index.aspx.cs
@@ -1,28 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
-public partial class Home : Page
+public partial class Home :  System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Page load logic, if any
+
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        // Logic for Pastry Boxes button click
-        Response.Redirect("Categories.aspx");
+        //TextBox1.Text = "Hello and Welcome";
     }
 
-    protected void Button2_Click(object sender, EventArgs e)
+    protected void Button1_Click1(object sender, EventArgs e)
     {
-        // Logic for Sweet Boxes button click
-        Response.Redirect("Categories.aspx");
-    }
-
-    protected void Button3_Click(object sender, EventArgs e)
-    {
-        // Logic for Cake Boxes button click
         Response.Redirect("Categories.aspx");
     }
 }
